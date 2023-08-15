1ee338fd267a4c067f4b50fd56641be0ae7813cf
Brahima Traore <makassi@BrahimaMac.local>
2023-08-15 16:22:42 -0400

    module sharing & async loading

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const cartContent = `<div> You have ${faker.datatype.number()} in your cart</div>`
-
-document.querySelector("#dev-cart").innerHTML = cartContent;
+import('./bootstrap');
